3948269a32042716a7ec112e9c5b3fbbc27d802c
Larissa Corrẽa <larimscorrea@gmail.com>
2023-05-13 20:44:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+  
+    <link rel="stylesheet" href="style.css" />
+    <title>Document</title>
+</head>
+<body>
+    <div id="corpo">
+    <h1>Choice the game</h1>
+    <table class="listagem">
+        <tr><td><td>cod<td>name<td>adm 
+        <tr><td><td>cod<td>name<td>adm 
+        <tr><td><td>cod<td>name<td>adm 
+        <tr><td><td>cod<td>name<td>adm 
+        <tr><td><td>cod<td>name<td>adm 
+
+
+    </table>
+    </div>
+    
+</body>
+</html>
