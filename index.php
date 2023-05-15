a3c2a713329c06dae44c8b802e719e32e1b0f950
Larissa Corrẽa <larimscorrea@gmail.com>
2023-05-14 21:15:51 -0300

    php

--- index.php
+++ index.php
@@ -10,12 +10,21 @@
     <div id="corpo">
     <h1>Choice the game</h1>
     <table class="listagem">
-        <tr><td><td>cod<td>name<td>adm 
-        <tr><td><td>cod<td>name<td>adm 
-        <tr><td><td>cod<td>name<td>adm 
-        <tr><td><td>cod<td>name<td>adm 
-        <tr><td><td>cod<td>name<td>adm 
-
+    <tr>
+    <th>code</th>
+    <th>admin</th>
+    <th>description</th>
+  </tr>
+  <tr>
+    <td>0001</td>
+    <td>larissa</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>0003</td>
+    <td>others</td>
+    <td>Mexico</td>
+  </tr>
 
     </table>
     </div>
